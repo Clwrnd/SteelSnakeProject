34615fda52b3ea3d5e0870b150b8ff0211fe967c
Clwrnd <127970668+Clwrnd@users.noreply.github.com>
2023-06-09 07:00:04 +0000

    Ajout du projet

---
+++ SerpentDeFerProject/src/main/java/fr/idmont/candw/serpentdeferproject/SystemInfo.java
@@ -0,0 +1,13 @@
+package fr.idmont.candw.serpentdeferproject;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ SerpentDeFerProject/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module fr.idmont.candw.serpentdeferproject {
+    requires javafx.controls;
+    exports fr.idmont.candw.serpentdeferproject;
+}
